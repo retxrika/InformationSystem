f796385407278286fde51d1b3774b290e1d866b7
olegbelsky12378 <olegbelsky12378@gmail.com>
2021-01-09 16:39:07 +0500

    Заполнил все остальные классы описывающие сотрудников

--- InformationSystem/Models/Employees/CEO.cs
+++ InformationSystem/Models/Employees/CEO.cs
@@ -5,5 +5,16 @@
     /// </summary>
     internal class CEO : Employee
     {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="id">Employee ID.</param>
+        /// <param name="name">Employee name.</param>
+        /// <param name="age">Employee age.</param>
+        /// <param name="projects">The number of projects an employee has.</param>
+        /// <param name="salary">Employee salary.</param>
+        public CEO(ushort id, string name, byte age, byte projects, uint salary)
+            : base(id, name, age, Position.CEO, projects, salary)
+        { }
     }
 }
--- InformationSystem/Models/Employees/Intern.cs
+++ InformationSystem/Models/Employees/Intern.cs
@@ -5,5 +5,16 @@
     /// </summary>
     internal class Intern : Employee
     {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="id">Employee ID.</param>
+        /// <param name="name">Employee name.</param>
+        /// <param name="age">Employee age.</param>
+        /// <param name="projects">The number of projects an employee has.</param>
+        /// <param name="salary">Employee salary.</param>
+        public Intern(ushort id, string name, byte age, byte projects, uint salary)
+            : base(id, name, age, Position.Intern, projects, salary)
+        { }
     }
 }
