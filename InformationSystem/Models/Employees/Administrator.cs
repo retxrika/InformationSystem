e42731addab4050acb3f212bcb5199388a0b6f0b
olegbelsky12378 <olegbelsky12378@gmail.com>
2021-01-09 16:34:49 +0500

    Заполнил класс описывающий администратора

--- InformationSystem/Models/Employees/Administrator.cs
+++ InformationSystem/Models/Employees/Administrator.cs
@@ -1,9 +1,20 @@
 ﻿namespace InformationSystem.Models.Employees
 {
     /// <summary>
-    /// Класс описывающий 
+    /// Класс описывающий администратора.
     /// </summary>
     internal class Administrator : Employee
     {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="id">Employee ID.</param>
+        /// <param name="name">Employee name.</param>
+        /// <param name="age">Employee age.</param>
+        /// <param name="projects">The number of projects an employee has.</param>
+        /// <param name="salary">Employee salary.</param>
+        public Administrator(ushort id, string name, byte age, byte projects, uint salary) 
+            : base(id, name, age, Position.Administrator, projects, salary)
+        { }
     }
 }
